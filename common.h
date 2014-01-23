207477cade79204b5d48005aca2673da96596a9a
王大狗 <zhainx@gmail.com>
2014-01-23 14:05:57 +0800

    add matrix rotate

---
+++ common.h
@@ -0,0 +1,64 @@
+/*
+ 
+                                           .. .vr       
+                                         qBMBBBMBMY     
+                                        8BBBBBOBMBMv    
+                                      iMBMM5vOY:BMBBv        
+                      .r,             OBM;   .: rBBBBBY     
+                      vUL             7BB   .;7. LBMMBBM.   
+                     .@Wwz.           :uvir .i:.iLMOMOBM..  
+                      vv::r;             iY. ...rv,@arqiao. 
+                       Li. i:             v:.::::7vOBBMBL.. 
+                       ,i7: vSUi,         :M7.:.,:u08OP. .  
+                         .N2k5u1ju7,..     BMGiiL7   ,i,i.  
+                          :rLjFYjvjLY7r::.  ;v  vr... rE8q;.:,, 
+                         751jSLXPFu5uU@guohezou.,1vjY2E8@Yizero.    
+                         BB:FMu rkM8Eq0PFjF15FZ0Xu15F25uuLuu25Gi.   
+                       ivSvvXL    :v58ZOGZXF2UUkFSFkU1u125uUJUUZ,   
+                     :@kevensun.      ,iY20GOXSUXkSuS2F5XXkUX5SEv.  
+                 .:i0BMBMBBOOBMUi;,        ,;8PkFP5NkPXkFqPEqqkZu.  
+               .rqMqBBMOMMBMBBBM .           @kexianli.S11kFSU5q5   
+             .7BBOi1L1MM8BBBOMBB..,          8kqS52XkkU1Uqkk1kUEJ   
+             .;MBZ;iiMBMBMMOBBBu ,           1OkS1F1X5kPP112F51kU   
+               .rPY  OMBMBBBMBB2 ,.          rME5SSSFk1XPqFNkSUPZ,.
+                      ;;JuBML::r:.:.,,        SZPX0SXSP5kXGNP15UBr.
+                          L,    :@huhao.      :MNZqNXqSqXk2E0PSXPE .
+                      viLBX.,,v8Bj. i:r7:,     2Zkqq0XXSNN0NOXXSXOU 
+                    :r2. rMBGBMGi .7Y, 1i::i   vO0PMNNSXXEqP@Secbone.
+                    .i1r. .jkY,    vE. iY....  20Fq0q5X5F1S2F22uuv1M; 
+
+*/
+/*
+ * =====================================================================================
+ *
+ *       Filename:  common.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014/01/22 13时41分18秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  bigdog (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#ifndef yes
+#define yes 0
+#endif 
+#ifndef no
+#define no -1 
+#endif 
+
+#endif
---
+++ matrix_rotate/mtx_rt.c
@@ -0,0 +1,135 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  mtx_rt.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014/01/22 13时39分28秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  bigdog (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "../common.h"
+
+/* 
+ * a : transpose
+ * b : 1) left : change rows (1, n), (2, n-1) ..
+ *     2) right: change columns
+ */ 
+
+static inline void swap(uint32_t *a, uint32_t *b) {
+    uint32_t t = *a; *a = *b; *b = t;
+}
+
+int transpose(uint32_t *mtx, uint8_t n) {
+    if (NULL == mtx) return no;
+    int i, j;
+
+    for (i=0; i<n; ++i) {
+        for (j=i; j<n; ++j) {
+            // mtx[i][j], mtx[j][i]
+            swap( mtx+i*n+j, mtx+j*n+i);
+        }
+    }
+
+    return yes;
+}
+
+int rotate_right(uint32_t *mtx, uint8_t n ) {
+    if (NULL == mtx) return no;
+    int i, j;
+    int h = n / 2;
+
+    if (transpose(mtx, n) == no) {
+        return no;
+    }
+
+    // change columns
+    for (i=0; i<n; ++i)  {
+        for (j=0; j<h; ++j) {
+            swap(mtx+i*n+j, mtx+i*n+n-1-j);
+        }
+    }
+    return yes;
+}
+
+int rotate_left(uint32_t *mtx, uint8_t n) {
+    if (NULL == mtx) return no;
+    int i, j;
+    int h = n / 2;
+
+    if (transpose(mtx, n) == no) {
+        return no;
+    }
+
+    // change rows
+    for (j=0; j<n; ++j) {
+        for (i=0; i<h; ++i) {
+            // swap (mtx[i][j], mtx[n-1-i][j])
+            swap(mtx+i*n+j, mtx+(n-1-i)*n+j);
+        }
+    }
+
+    return yes;
+}
+
+
+#ifdef __MTX_DEBUG
+
+void print(uint32_t *mtx, uint8_t n) {
+    if (NULL == mtx) return;
+    int i, j;
+    printf ( "===============================\n" );
+    for (i=0; i<n; ++i) {
+        for (j=0; j<n; ++j) {
+            printf ( "%d\t", *(mtx+(i*n) + j) );
+        }
+        printf ( "\n" );
+    }
+    printf ( "===============================\n\n" );
+}
+
+
+void rotate_debug(uint8_t size, uint8_t right) {
+    if (size < 1) return;
+
+    uint32_t *mtx = malloc(sizeof(uint32_t)*(size*size));
+    for (int i=0; i<size*size; ++i) {
+        mtx[i] = i+1;
+    }
+
+    // original N*N
+    printf ( "Original [ N*N ] : \n" );
+    print(mtx, size);
+
+    if (right == 1) {
+        rotate_right(mtx, size);
+    } else {
+        rotate_left(mtx, size);
+    }
+
+    // result N*N
+    printf ( "Rotate %s [ N*N ] :\n", right == 1 ? "right" : "left" );
+    print(mtx, size);
+
+    free(mtx);
+}
+
+
+int main ( int argc, char *argv[] ) {
+#define RIGHT 1
+#define LEFT  0
+    rotate_debug(7, RIGHT);
+    rotate_debug(8, LEFT);
+
+    return EXIT_SUCCESS;
+}		
+
+#endif
